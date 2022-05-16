732ac5a43efdffb734f9c5ee144b212c3e564644
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-05-16 15:39:14 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { FilePond, registerPlugin } from "react-filepond";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import "filepond/dist/filepond.min.css";
+
+import {
+  makeDeleteRequest,
+  makeUploadRequest,
+} from "./cloudinary/cloudinaryHelper";
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 function App() {
+  const [files, setFiles] = useState([]);
+
+  const revert = (token, successCallback, errorCallback) => {
+    makeDeleteRequest({
+      token,
+      successCallback,
+      errorCallback,
+    });
+  };
+
+  const process = (
+  fieldName,
+    file,
+    metadata,
+    load,
+    error,
+    progress,
+    abort,
+    transfer,
+    options
+  ) => {
+    const abortRequest = makeUploadRequest({
+      file,
+      fieldName,
+      successCallback: load,
+      errorCallback: error,
+      progressCallback: progress,
+    });
+
+    return {
+      abort: () => {
+        abortRequest();
+        abort();
+      },
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "80%", margin: "auto", padding: "1%" }}>
+      <FilePond
+        files={files}
+        acceptedFileTypes="image/*"
+        onupdatefiles={setFiles}
+        allowMultiple={true}
+        server={{ process, revert }}
+        name="file"
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
     </div>
   );
 }
-
 export default App;
